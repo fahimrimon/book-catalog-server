b24a445260a65ca6858396ab9054593e3257e6c4
fahimrimon <fahimrimon21@gmail.com>
2023-07-24 13:44:19 +0600

    implement get api

--- index.js
+++ index.js
@@ -13,28 +13,29 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
-});
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1,
+  });
 
-async function run() {
+const run = async () => {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    const db = client.db('book-catalog');
+    const booksCollection = db.collection('books');
+
+    app.get('/books', async (req, res) => {
+      const cursor = booksCollection.find({});
+      const books = await cursor.toArray();
+      console.log(books)
+      res.send({ status: true, data: books });
+      
+    });
   } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
-run().catch(console.dir);
+
+run().catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
